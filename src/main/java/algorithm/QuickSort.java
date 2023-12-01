ee90b3eff2f2c3958bfa83b603a76a9281213bbe
gendogo <whj.bnm@qq.com>
2023-12-01 21:16:28 +0800

    RadixSort

--- src/main/java/algorithm/QuickSort.java
+++ src/main/java/algorithm/QuickSort.java
@@ -1,46 +1,15 @@
 package algorithm;
 
+import java.io.*;
+import java.util.Arrays;
+
 /**
  * @Description:
  * @Author: whj
  * @Date: 2023-11-29 22:13
  */
 public class QuickSort {
-    public static int first;
-    public static int last;
-    public static int[] arr;
 
-    private static void quicksort(int l, int r) {
-        if (l >= r) {
-            return;
-        }
-        //不能写成(Math.random() * arr.length),必须要在当前分区选择x，而不是整体
-        int x = arr[l + (int) (Math.random() * (r - l + 1))];
-        partition(l, x, r);
-        int left = first - 1;
-        int right = last + 1;
-        quicksort(l, left);
-        quicksort(right, r);
-    }
 
-    private static void partition(int l, int x, int r) {
-        first = l;
-        int i = l;
-        last = r;
-        while (i <= last) {
-            if (arr[i] == x) {
-                i++;
-            } else if (arr[i] < x) {
-                swap(first++, i++);
-            } else {
-                swap(last--, i);
-            }
-        }
-    }
 
-    private static void swap(int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
 }
